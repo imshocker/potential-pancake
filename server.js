7c5a6608c037974d6265c28ed3208c0a3d517af0
Shocker <zacsmcdowell@gmail.com>
2023-10-31 13:49:53 -0400

    updating routes

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const path = require('path');
-
+const routes = require('./controllers');
 
 const app = express();
 const PORT = process.env.PORT || 3002;
@@ -10,8 +10,16 @@ app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 
 // GET Route for homepage
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/index.html'))
+})
+
 app.get('*', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+
+app.get('/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
 
 app.use(routes);
