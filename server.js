783eb46ea038f8c0c63ba2273cd3972b209dfcff
Shocker <zacsmcdowell@gmail.com>
2023-10-31 05:28:04 -0400

    added to server.js

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const path = require('path');
+
+
+const app = express();
+const PORT = process.env.PORT || 3002;
+
+app.use(express.json());
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+
+// GET Route for homepage
+app.get('*', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+
+app.use(routes);
+
+app.listen(PORT, () => console.log('Now listening'));
